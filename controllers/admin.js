5e2e5e1887fe97c3647def4b436b0f68f6d2ac7c
hasib999 <45915540+hasib999@users.noreply.github.com>
2020-08-16 19:45:58 +0600

    employee Update done

--- controllers/admin.js
+++ controllers/admin.js
@@ -15,5 +15,37 @@ router.get('/',function(req,res){
         res.redirect("/login");
     }
 })
+router.get('/Update/:id',function(req,res)
+{
+    if(req.session.status==1)
+    {
+        var user={
+            username: req.params.id
+        }
+        login.get(user,function(result)
+        {
+            var errors = validationResult(req);
+            res.render('admin/update/index',{list:result});
+        });
+    }
+    
+});
 
+router.post('/Update/:id',function(req,res)
+{
+    if(req.session.status==1)
+    {
+        var user=
+    {
+        
+        password: req.body.password,
+        name: req.body.name,
+        phone: req.body.phone,
+        username: req.body.username
+    }
+    login.update(user,function(result)
+    {
+        res.redirect('/admin/emplist');
+    })
+    }
 module.exports=router;
